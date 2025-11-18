8ca2e704ff5090843865f2c204f1ae8226796610
zurc <afonsobpcruz@gmail.com>
2025-11-18 10:44:48 +0000

    no errors now

--- Aulas/Labs/W1L1/Lab 1 - Part B - C revision/lab1.c
+++ Aulas/Labs/W1L1/Lab 1 - Part B - C revision/lab1.c
@@ -5,13 +5,13 @@
 int main(){
     char line[100];
     int n;
-    printf("What is your student number");
+    printf("What is your student number ");
     fgets(line, 100, stdin);
     if (sscanf(line, "%d", &n) != 1){
         printf("invalid Number\n");
         exit(-1);
     }
     srandom(n);
-    printf("your own random %d\n", random());   
+    printf("your own random %ld\n", random());   
     exit(0);
 }
