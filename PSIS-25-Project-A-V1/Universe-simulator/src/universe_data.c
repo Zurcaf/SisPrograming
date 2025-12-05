33c3995d0053199739fa431b52ecc93616876eaa
zurc <afonsobpcruz@gmail.com>
2025-12-05 13:10:00 +0000

    inicio da abstração

--- PSIS-25-Project-A-V1/Universe-simulator/src/universe_data.c
+++ PSIS-25-Project-A-V1/Universe-simulator/src/universe_data.c
@@ -1,17 +1,21 @@
 #include "../head/universe_data.h"
-planet_t* init_planets(int n, int width, int height) {
-    planet_t* planets = malloc(n * sizeof(planet_t));
-    if (planets == NULL) {
-        fprintf(stderr, "Memory allocation failed for planets.\n");
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < n; i++) {
-        planets[i].x = rand() % width;
-        planets[i].y = rand() % height;
-        planets[i].mass = 10;   //10 mass units
-    }
-    return planets;
-}
+
+typedef struct vector {
+    float amplitude;
+    float angle;
+} vector_t;
+
+typedef struct planet {
+    float x, y;
+    float mass;
+} planet_t;
+
+typedef struct trash {
+    float x, y;
+    vector_t velocity;
+    vector_t acceleration;
+    float mass;
+} trash_t;
 
 trash_t* init_trash(int n, int width, int height) {
     trash_t* trash = malloc(n * sizeof(trash_t));
@@ -40,4 +44,38 @@ void addTrash(int n_trash, trash_t* trash, int width, int height)
     trash[n_trash].velocity.angle = 0;
     trash[n_trash].acceleration.amplitude = 0;
     trash[n_trash].acceleration.angle = 0;
+}
+
+int get_trash_cords(int trash_index, trash_t* trash, float* x, float* y) {
+    if (trash == NULL || x == NULL || y == NULL) {
+        return -1; // Error: null pointer
+    }
+    *x = trash[trash_index].x;
+    *y = trash[trash_index].y;
+    return 0; // Success
+}
+
+planet_t* init_planets(int n, int width, int height) {
+    planet_t* planets = malloc(n * sizeof(planet_t));
+    if (planets == NULL) {
+        fprintf(stderr, "Memory allocation failed for planets.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < n; i++) {
+        planets[i].x = rand() % width;
+        planets[i].y = rand() % height;
+        planets[i].mass = 10;   //10 mass units
+    }
+    return planets;
+}
+
+int get_planet_cords(int planet_index, planet_t *planets, float *x, float *y)
+{
+    if (planets == NULL || x == NULL || y == NULL)
+    {
+        return -1; // Error: null pointer
+    }
+    *x = planets[planet_index].x;
+    *y = planets[planet_index].y;
+    return 0; // Success
 }
